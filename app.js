2148c6920c7897bdab29bb09e2acc66a1cbe09d6
haimhugi <65680555+haimhugi@users.noreply.github.com>
2022-06-10 15:49:43 +0300

    fix err

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ const mongoose = require('mongoose');
 const app = express();
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,DELETE');
+
+    next();
+});
+
 app.use('/api/users', usersRoutes);
 
 app.use((req, res, next) => {
